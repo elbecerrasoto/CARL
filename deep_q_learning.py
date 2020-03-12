fe7a943ca0a229c495c2351f19d8b7bb9a905686
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2020-03-12 17:28:41 -0600

    wk DQN

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -23,6 +23,18 @@ REPLAY_SIZE = 10000
 REPLAY_START_SIZE = 1000
 BATCH_SIZE = 32
 
+GAMMA = 0.95
+# Test GAMMA decay
+# 1*(GAMMA**np.arange(0,300))
+
+LEARNING_RATE = 0.0001
+
+# Epsilon decreses linearly by epoch
+EPSILON_START = 1.00
+EPSILON_END = 0.00
+
+EPOCHS = 10
+
 Experience = collections.namedtuple('Experience',
                                     field_names=('state', 'action', 'reward', 'done', 'new_state'))
 
@@ -120,12 +132,12 @@ class Agent:
         self._reset()
 
     def _reset(self):
-        self.state = env.reset()
+        self.state = self.env.reset()
         self.total_reward = 0.0
 
     def play_step(self, net, epsilon=0.0):
         if np.random.random() < epsilon:
-            actions_set = list(env.actions_set)
+            actions_set = list(self.env.actions_set)
             action = np.random.choice(actions_set)
         else:
             grid, position = observations_to_tensors([self.state])
@@ -146,82 +158,79 @@ class Agent:
             self._reset()
         return self.total_reward
 
-def calc_loss(batch, net, tgt_net, device="cpu"):
-    states, actions, rewards, dones, next_states = batch
-
-    states_v = torch.tensor(states).to(device)
-    next_states_v = torch.tensor(next_states).to(device)
-    actions_v = torch.tensor(actions).to(device)
-    rewards_v = torch.tensor(rewards).to(device)
-    done_mask = torch.ByteTensor(dones).to(device)
-
-    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
-    next_state_values = tgt_net(next_states_v).max(1)[0]
-    next_state_values[done_mask] = 0.0
-    next_state_values = next_state_values.detach()
-
-    expected_state_action_values = next_state_values * GAMMA + rewards_v
-    return nn.MSELoss()(state_action_values, expected_state_action_values)
-
-
 # Testing agent
-obs = ENV.reset()
-tgt_net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
-
-grid, position = observations_to_tensors([obs])
-net(grid, position)
-
+# obs = ENV.reset()
+net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
 agent = Agent(env = ENV, exp_buffer = ReplayMemory(REPLAY_SIZE))
 
-agent.env.render()
+# agent.env.render()
 for step_idx in range(100):
     agent.play_step(net, epsilon=0.10)
+    # agent.env.render()
+
+def calc_loss(batch, net, tgt_net):
+    states, actions, rewards, dones, next_states = batch
+    
+    grids, positions = observations_to_tensors(states)
+    grids_next, positions_next = observations_to_tensors(next_states)
+    
+    actions_v = torch.tensor(actions)
+    rewards_v = torch.tensor(rewards)
+    done_mask = torch.BoolTensor(dones)
+
+    # Quality of the taken actions
+    state_action_values = net(grids, positions).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+    
+    # Next state max Q(s,a)
+    next_state_values = tgt_net(grids_next, positions_next).max(1)[0]
+    # The value of ended episode is 0
+    next_state_values[done_mask] = 0.0
+    
+    # Calculate target
+    expected_state_action_values = rewards_v + GAMMA * next_state_values
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+# Testing loss calculation
+net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
+tgt_net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
+calc_loss(agent.exp_buffer.sample(2), net, tgt_net)
+
+count = 0
+for param in net.parameters():
+    # print(param)
+    count += 1
+print(count)
+
+#if __name__ == 'main':
+# Initializations
+# Non-Linear functions approximators
+net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
+tgt_net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
+
+buffer = ReplayMemory(REPLAY_SIZE)
+agent = Agent(ENV, buffer)
+
+optimizer = optim.Adam(net.parameters(), lr = LEARNING_RATE)
+# Sample using e-greedy
+
+# epsilon decreseases linearly by epoch
+epsilon_schedule = np.linspace(EPSILON_START, EPSILON_END, EPOCHS)
+for epoch, epsilon in enumerate(epsilon_schedule):
+    print(f'epoch: #{epoch} \t epsilon: {epsilon}')
+    agent.play_step(net, epsilon)
     agent.env.render()
+        
 
-tgt_net = Net(n_actions=9, n_row=9, n_col=9)
-batch = agent.exp_buffer.sample(2)
+for epsilon in np.linspace(EPSILON_START, EPSILON_END, EPOCHS)
 
-states, actions, rewards, dones, next_states = batch
-
-grids, positions = observations_to_tensors(states)
-grids_next, positions_next = observations_to_tensors(next_states)
-
-actions_v = torch.tensor(actions)
-rewards_v = torch.tensor(rewards)
-done_mask = torch.ByteTensor(dones)
-
-
-# Quality of the taken actions
-# The gather is differentiable
-state_action_values = net(grids, positions).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
-
-next_state_values = tgt_net(next_states_v).max(1)[0]
-next_state_values[done_mask] = 0.0
-next_state_values = next_state_values.detach()
-
-expected_state_action_values = next_state_values * GAMMA + rewards_v
-return nn.MSELoss()(state_action_values, expected_state_action_values)
-
-
-
-
-
-if __name__ == '__main__':
-    # Sample with epsilon greedy
-    pass
-
-
-
-
+np.linspace(EPSILON_START, EPSILON_END, EPOCHS)
+enumerate(np.linspace(0, 10, 37))
 
 if __name__ == "__main__":
+    net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
+    tgt_net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
 
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
-    tgt_net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
-    writer = SummaryWriter(comment="-" + args.env)
-    print(net)
-
-    buffer = ExperienceBuffer(REPLAY_SIZE)
+    buffer = ReplayMemory(REPLAY_SIZE)
     agent = Agent(env, buffer)
     epsilon = EPSILON_START
 
@@ -235,37 +244,43 @@ if __name__ == "__main__":
     while True:
         frame_idx += 1
         epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
-
+        
+        # Play an episode
+        # Using epsilon-greedy
         reward = agent.play_step(net, epsilon, device=device)
+        # At the end the of the episode
         if reward is not None:
             total_rewards.append(reward)
-            speed = (frame_idx - ts_frame) / (time.time() - ts)
-            ts_frame = frame_idx
-            ts = time.time()
+            # Get the mean of the last 100
             mean_reward = np.mean(total_rewards[-100:])
-            print("%d: done %d games, mean reward %.3f, eps %.2f, speed %.2f f/s" % (
-                frame_idx, len(total_rewards), mean_reward, epsilon,
-                speed
-            ))
+            # Save if moving the mean forward
+            # Saving the best network so far
             if best_mean_reward is None or best_mean_reward < mean_reward:
+                # Saving the best network so far
                 torch.save(net.state_dict(), args.env + "-best.dat")
                 if best_mean_reward is not None:
                     print("Best mean reward updated %.3f -> %.3f, model saved" % (best_mean_reward, mean_reward))
                 best_mean_reward = mean_reward
+            # Solved if reward threshold is met
             if mean_reward > args.reward:
                 print("Solved in %d frames!" % frame_idx)
                 break
-
+        
+        # Don't train yet, if the start size hasnt been met
         if len(buffer) < REPLAY_START_SIZE:
-            continue
-
+            continue 
+        # Syncronize net and target net
         if frame_idx % SYNC_TARGET_FRAMES == 0:
             tgt_net.load_state_dict(net.state_dict())
-
+        # Zeros the grads to proper backpropagation
         optimizer.zero_grad()
+        # Get the batch from replay memory
         batch = buffer.sample(BATCH_SIZE)
+        # Get the loss
         loss_t = calc_loss(batch, net, tgt_net, device=device)
+        # Apply backprop
         loss_t.backward()
+        # Actualize weights
         optimizer.step()
     writer.close()
 
