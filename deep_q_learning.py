e8cf5defca25922d3fdd673d064feef93b5259f7
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2020-03-25 18:55:11 -0700

    Easy DQN

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -20,81 +20,48 @@ writer = SummaryWriter('runs/DQN_helicopter')
 
 import helicopter
 
-N_ROW = 8; N_COL=8; P_FIRE=0.01; P_TREE=0.30
+N_ROW = 1; N_COL=8; P_FIRE=0.01; P_TREE=0.30
 ENV = helicopter.Helicopter(n_row = N_ROW, n_col = N_COL,
                             p_fire = P_FIRE, p_tree = P_TREE)
-REPLAY_SIZE = 10000
+REPLAY_SIZE = 1000
 # Start learning at this size of the replay memory
-REPLAY_START_SIZE = 100
-BATCH_SIZE = 32
-
-GAMMA = 0.95
-
+REPLAY_START_SIZE = 200
+BATCH_SIZE = 8
+GAMMA = 0.75
 LEARNING_RATE = 0.0001
+EPSILON = 0.10
 
-# Epsilon decreses linearly by epoch
-EPSILON_START = 1.00
-EPSILON_END = 0.00
-
-EPOCHS = 200
+EPOCHS = 5001
 SYNC_TARGET = 100
 
 Experience = collections.namedtuple('Experience',
                                     field_names=('state', 'action', 'reward', 'done', 'new_state'))
 
+HIDDEN_12 = 64
+HIDDEN_23 = 32
+
 class Net(nn.Module):
-    def __init__(self, n_actions, n_row, n_col):
+    def __init__(self, n_actions, n_row, n_col, hidden_12, hidden_23):
         super(Net, self).__init__()
-        
-        # Leaves image size
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels = 1,
-                out_channels = 6,
-                kernel_size = (3,3),
-                stride = 1,
-                padding = 1),
-            nn.ReLU())
-        
-        # Halves image size
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels = 6,
-                out_channels = 12,
-                kernel_size = (3,3),
-                stride = 1,
-                padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        
-        # Add Helicopter position
-        self.fc12 = nn.Linear(12 * (n_row//2) * (n_col//2) + 2, 60)
-        self.fc23 = nn.Linear(60, 20)
+        self.hidden_12 = hidden_12
+        self.hidden_23 = hidden_23
+        # Flatten Lattice and Add Helicopter position
+        self.fc12 = nn.Linear(n_row * n_col + 2, self.hidden_12)
+        self.fc23 = nn.Linear(self.hidden_12, self.hidden_23)
         # Output layer
-        self.fc34 = nn.Linear(20, n_actions)
-        
+        self.fc34 = nn.Linear(self.hidden_23, n_actions)
     def forward(self, batch_grid, batch_position):
-        # Conv1
-        batch_grid = self.conv1(batch_grid)
-        # Conv2
-        batch_grid = self.conv2(batch_grid)
-        
         # Flatten
         # -1, copies batch dimension
         batch_grid = batch_grid.view(-1, self.num_flat_features(batch_grid))
-        
-        # Needs to add position info
-        # before passing to fully connected
-        batch_grid = torch.cat((batch_position, batch_grid), dim=1)
-        
+        # Adding position info
+        batch_grid = torch.cat((batch_position, batch_grid), dim=1)     
         # The start of the fully connected layers
         batch_grid = self.fc12(batch_grid)
         batch_grid = self.fc23(batch_grid)
-        
         # Output
         batch_grid = self.fc34(batch_grid)
         return batch_grid
-    
     def num_flat_features(self, x):
         size = x.size()[1:] # all dimensions, except batch
         num_features = 1
@@ -181,10 +148,26 @@ def calc_loss(batch, net, tgt_net):
     expected_state_action_values = rewards_v + GAMMA * next_state_values
     return nn.MSELoss()(state_action_values, expected_state_action_values)
 
+
+def get_epsilon_schedule(epochs, epsilon=0.05, exploring_time=0.15, epsilon_time=0.84):
+    assert exploring_time + epsilon_time <= 1.0
+    exploiting_time = 1.0 - (exploring_time + epsilon_time)
+    exploring_steps = np.floor(epochs*exploring_time)
+    epsilon_steps = np.floor(epochs*epsilon_time)
+    exploiting_steps = np.floor(epochs*exploiting_time)
+    exploring_v = np.repeat(1.0, exploring_steps)
+    epsilon_v = np.repeat(epsilon, epsilon_steps)
+    exploiting_v = np.repeat(0.0, exploiting_steps)
+    schedule = np.concatenate((exploring_v, epsilon_v, exploiting_v))
+    tail_len = epochs - len(schedule)
+    tail = np.repeat(0.0, tail_len)
+    schedule = np.concatenate((schedule, tail))
+    return schedule
+
 if __name__ == '__main__':
     # Initializations
-    net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
-    tgt_net = Net(n_actions=ENV.actions_cardinality, n_row=ENV.n_row, n_col=ENV.n_col)
+    net = Net(ENV.actions_cardinality, ENV.n_row, ENV.n_col, HIDDEN_12, HIDDEN_23)
+    tgt_net = Net(ENV.actions_cardinality, ENV.n_row, ENV.n_col, HIDDEN_12, HIDDEN_23)
     
     buffer = ReplayMemory(REPLAY_SIZE)
     agent = Agent(ENV, buffer)
@@ -192,38 +175,38 @@ if __name__ == '__main__':
     optimizer = optim.Adam(net.parameters(), lr = LEARNING_RATE)
     
     # epsilon decreseases linearly by epoch
-    epsilon_schedule = np.linspace(EPSILON_START, EPSILON_END, EPOCHS)
+    epsilon_schedule = get_epsilon_schedule(EPOCHS, EPSILON)
     
     # Fill the replay memory
     for filling_step in range(REPLAY_START_SIZE):
         agent.play_step(net, epsilon = 1.00)
-        print('.', end='')    
+        # print('.', end='')    
     print(f'\nFilled Replay Memory')
     
     rewards_x_steps = collections.deque()
-    sample_renders = collections.deque()
+    # sample_renders = collections.deque()
     
-    print('Starting to learn now', end='\n\n')
+    print('STARTING TO LEARN NOW', end='\n\n')
     # Start training
     for epoch, epsilon in enumerate(epsilon_schedule):
         writer.add_scalar('epsilon', epsilon, epoch)
-        print(f'.{epoch}',end='')
+        # print(f'.{epoch}',end='')
         # Syncronize net and target net
         if epoch % SYNC_TARGET == 0 and epoch != 0:
-            sample_renders.append(agent.env.render())
-            print(f'\n\n')
+            # sample_renders.append(agent.env.render())
+            # print(f'\n\n')
+            print(f'#### EPOCH: {epoch} ####')
             tgt_net.load_state_dict(net.state_dict())
-            rewards_x_steps = np.array(rewards_x_steps, dtype='float32')
-            mean_reward = rewards_x_steps.mean()
-            total_reward = rewards_x_steps.sum()
-            writer.add_scalar(f'mean_reward_{SYNC_TARGET}_steps', mean_reward, epoch)
+            rewards_x_steps_v = np.array(rewards_x_steps, dtype='float32')
+            total_reward = rewards_x_steps_v.sum()
+            mean_reward = rewards_x_steps_v.mean()
+            writer.add_scalar(f'mean_reward_per_step', mean_reward, epoch)
             writer.add_scalar(f'return_{SYNC_TARGET}_steps', total_reward, epoch)
-            print(f'mean reward per {SYNC_TARGET} steps:', mean_reward)
-            print(f'total reward per {SYNC_TARGET} steps: {total_reward}')
+            print(f'TOTAL REWARD per {SYNC_TARGET} steps: {total_reward}\n')
             rewards_x_steps = collections.deque()
-            torch.save(net.state_dict(), 'DQN_last.dat')
-            with open('renders.dat', 'wb') as file:
-                pickle.dump(sample_renders, file)
+            # torch.save(net.state_dict(), 'DQN_last.dat')
+            # with open('renders.dat', 'wb') as file:
+            #     pickle.dump(sample_renders, file)
         # Play a step
         # agent.env.render()
         reward = agent.play_step(net, epsilon)
@@ -237,9 +220,11 @@ if __name__ == '__main__':
         writer.add_scalar('loss', loss_t, epoch)
         loss_t.backward()
         optimizer.step()
+        
     states, __, __, __, __ = agent.exp_buffer.sample(BATCH_SIZE)
     grids, positions = observations_to_tensors(states)
     writer.add_graph(net, (grids, positions))
     writer.close()
     with open('net.dat', 'wb') as file:
         pickle.dump(net, file)
+    torch.save(net.state_dict(), 'DQN_params.dat')
